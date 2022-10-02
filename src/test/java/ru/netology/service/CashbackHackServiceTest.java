51896838e9a8d2e89fb1203b1d7525f28e427e80
Дарья Опякина <daryaopyakina@mail.ru>
2022-10-02 21:57:37 +0300

    New twst in branch testing

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -15,4 +15,22 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
 
     }
+    @Test
+    public void testRemainfull() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void testRemainnull() {
+        int amount = 0;
+        int expected = 1000;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
 }
